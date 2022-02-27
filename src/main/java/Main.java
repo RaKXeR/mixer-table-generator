bd8b4f5e8ffe7a521eef839e3b2fc6a384797c4e
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2022-02-27 05:41:46 +0000

    Move retry code to `RoundBuilder`
    - Refactored `RoundBuilder` so that `createRound` only returns properly formulated tables, instead of requiring the caller to constantly validate the output. Returns null when no valid round is found;
    - Refactored `Main` inline with these changes;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,41 +38,23 @@ public class Main {
         }
 
 
+        // Generate as many rounds as possible for the current player list
         List<Round> rounds = new LinkedList<>();
-        List<Player> players;
-        // Keep track of how many times we generated a round that had leftover players to prevent infinite loops
-        int retryCounter = 0;
-        do {
-            // Copy player list and use it to generate unique tables
-            players = new ArrayList<>(playerMap.values());
-
-            // Generate a round of tables with players who haven't played with eachother
-            Round round = RoundBuilder.createRound(players, 4);
-
-            // Try again if not all players fit in
-            if (players.size() > 0 || !round.hasAllPlayers(playerMap.values())) {
-                // Break out of loop if finding a set of tables is taking too many attempts
-                if (++retryCounter >= TableBuilder.TIMEOUT) {
-                    break;
-                }
-                players.clear();
-                continue;
-            }
-
-            if (round.size() > 0) {
-                //TODO: Ask user if they want to store changes or not
+        Round round;
+        while ((round = RoundBuilder.createRound(playerMap.values(), 4)) != null) {
+            // Generate a round of tables with players who haven't played with each other
                 rounds.add(round);
-            }
+                //TODO: Ask user if they want to store changes or not
 
-            // Mark table members as played with for each table now that we know this round is to be kept
-            for (Table table : round) {
-                for (Player player : table.getPlayers()) {
-                    player.markAsPlayedWith(table.getPlayers());
+                // Mark table members as played with for each table now that we know this round is to be kept
+                for (Table table : round) {
+                    for (Player player : table.getPlayers()) {
+                        player.markAsPlayedWith(table.getPlayers());
+                    }
                 }
-            }
+        };
 
-        } while (players.size() == 0);
-
+        // List generated rounds
         int i = 0;
         for (Collection<Table> tableCollection : rounds) {
             System.out.printf("Round %d (%d tables):\n", ++i, tableCollection.size());
--- src/main/java/model/RoundBuilder.java
+++ src/main/java/model/RoundBuilder.java
@@ -1,16 +1,53 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class RoundBuilder {
 
     /** Creates a new round with randomly selected players from the given list while ensuring that
      * none of the seated players have been seated together before. Does not mark each player as played with.
+     * @param playerList list of available players for seating. Players are removed when seated at a table
+     * @param maxTableSize preferred table size, sets the limit for player capacity for each table
+     * @return a new round with random unrelated players on each table, or null if not all players can be seated
+     */
+    public static Round createRound(Collection<Player> playerList, int maxTableSize) {
+        List<Player> players;
+        // Keep track of how many times we generated a round that had leftover players to prevent infinite loops
+        int retryCounter = 0;
+        do {
+            // Copy player list and use it to generate unique tables
+            players = new ArrayList<>(playerList);
+
+            // Generate a round of tables with players who haven't played with each other
+            Round round = createUncheckedRound(players, maxTableSize);
+
+            // Try again if not all players fit in
+            if (players.size() > 0 || !round.hasAllPlayers(playerList)) {
+                // Break out of loop if finding a set of tables is taking too many attempts
+                if (++retryCounter >= TableBuilder.TIMEOUT) {
+                    break;
+                }
+                players.clear();
+                continue;
+            }
+            if (round.size() > 0) {
+                return round;
+            }
+
+        } while (players.size() == 0);
+
+        return null;
+    }
+
+    /** Creates a new round with randomly selected players from the given list while ensuring that
+     * none of the seated players have been seated together before. Does not mark each player as played with.
      * @param players list of available players for seating. Players are removed when seated at a table
      * @param maxTableSize preferred table size, sets the limit for player capacity for each table
-     * @return a new round with random unrelated players on each table
+     * @return a new round with random unrelated players on each table. Doesn't ensure all players are seated
      */
-    public static Round createRound(List<Player> players, int maxTableSize) {
+    private static Round createUncheckedRound(List<Player> players, int maxTableSize) {
         Round round = new Round();
         // Generate tables until one comes out null, meaning we ran out of players
         Table table = TableBuilder.createTable(players, maxTableSize);
